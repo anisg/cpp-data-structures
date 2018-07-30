3be3df2bc9db37e6693f2fa0bf911498ce265eae
macbook Anis <anis@anis.local>
2018-07-30 17:45:54 +0200

    update trie

--- Trie.cpp
+++ Trie.cpp
@@ -4,29 +4,31 @@ using namespace std;
 
 // Trie : insert(s), find(s), remove(s,decr)
 template <typename X, typename S=string, typename C=char>struct Trie {
-	struct Node { int count = 0; X d; unordered_map<C,Node*> m; };
-	typedef int (*lengthFunc)(const S &); typedef C(*accessFunc)(const S &, int);
-	Node *root; lengthFunc lf; accessFunc af;
-	Trie(lengthFunc lf = [](const S & s){return int(s.size());}, accessFunc af = ([](const S &s, int i){return s[i];})) : root(new Node()), lf(lf), af(af) {}
+	struct Node { int count = 0; int childs = 0; X d; unordered_map<C,Node*> m;};
+	typedef int (*lFunc)(const S &); typedef C(*aFunc)(const S &, int);
+	Node *root; lFunc lf; aFunc af;
+	Trie(aFunc af = [](const S &s, int i){return s[i];}, lFunc lf = [](const S & s){return int(s.size());}) : root(new Node()), lf(lf), af(af) {}
 	void insert(const S & s) {
-		Node *z = root;
-		int n = lf(s);
+		Node *z = root; int n = lf(s);
 		for (int i = 0; i < n; i++){
 			C c = af(s, i);
 			if (!z->m[c]) z->m[c] = new Node();
+			z->childs += 1;
 			z = z->m[c];
 		}
 		z->count += 1;
 	}
-	bool remove(const S & s, bool decr = false){
-		Node *z;
-		if (!(z = find(s))) return false;
+	bool remove(const S & s, bool decr = true){
+		Node *z = root; int n = lf(s);
+		for (int i = 0; i < n; i++){
+			C c = af(s, i); if (!z->m[c]) return false;
+			z->childs -= 1; z = z->m[c];
+		}
 		z->count = (decr) ? z->count - 1 : 0;
 		return true;
 	}
 	Node *find(const S & s, int m = -1){
-		Node *z = root;
-		int n = lf(s);
+		Node *z = root; int n = lf(s);
 		for (int i = 0; i < n; i++){
 			C c = af(s, i);
 			if (!z->m[c]) return 0x0;
